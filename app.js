898e12b67ed3d20c1d7785d294f00314c107454b
tugceakkaya <tgcakkaya1@gmail.com>
2021-08-29 18:37:56 +0300

    new commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use('/abc', movies);
 
 
 app.get('/', (req,res) => {
-    res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment!!');
+    res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment ın Heroku');
 })
 
 
