d3d8df64a83beb69f120800d169348161c492b8c
tugceakkaya <tgcakkaya1@gmail.com>
2021-08-31 14:02:09 +0300

    tugce

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use('/abc', movies);
 
 
 app.get('/', (req,res) => {
-    res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment ın Heroku');
+    res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment ın Heroku tugce');
 })
 
 
