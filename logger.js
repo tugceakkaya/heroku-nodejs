a3d0d59dd289dfb6f970b726d6e71309d4e77e76
tugceakkaya <tgcakkaya1@gmail.com>
2021-09-02 19:35:32 +0300

    msg

--- logger.js
+++
@@ -1,9 +0,0 @@
-function log (message)
-{
-    console.log(message);
-}
-
-var abc = "Value";
-
-module.exports.log = log;
-module.exports.xyz = abc;
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome" });
 });
 
 // set port, listen for requests
